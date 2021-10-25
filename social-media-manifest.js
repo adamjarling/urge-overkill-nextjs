905c8743de806d0e2c2c60ac48af1a52d9e8ae9d
Adam J. Arling <aarling@gmail.com>
2021-10-25 17:25:15 -0500

    Update facebook link

--- social-media-manifest.js
+++ social-media-manifest.js
@@ -1,7 +1,7 @@
 export const distributors = {
   amazonMusic: `https://music.amazon.com/artists/B08BR4TXGF/furbaby?marketplaceId=ATVPDKIKX0DER&musicTerritory=US`,
   appleMusic: `https://music.apple.com/us/artist/urge-overkill/14773220`,
-  facebook: `http://www.facebook.com/Urgeoverkill#!/urgeoverkillmusic`,
+  facebook: `https://www.facebook.com/urgeoverkillmusic`,
   instagram: `https://www.instagram.com/urgeoverkill`,
   spotify: `https://open.spotify.com/artist/5LEUigTSXpwrtERRcSW1N4?si=tNigonZfQqeYKmofhbrSZQ`,
   youTubeChannel: ``,
