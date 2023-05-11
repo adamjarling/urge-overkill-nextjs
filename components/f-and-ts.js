909419e1c7bb795b3ae0ca6531701e596fea7baf
Adam J. Arling <aarling@gmail.com>
2023-05-11 08:53:18 -0500

    Cleanup files

--- components/f-and-ts.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function FandTS() {
-  return (
-    <span className="font-sans tracking-widest uppercase">
-      FURBABY &amp; the TIGHT SPACES
-    </span>
-  );
-}
