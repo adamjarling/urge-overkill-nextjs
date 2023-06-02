318f6dd6c6e326e305fdb6c85dbcc8a338319819
Adam J. Arling <aarling@gmail.com>
2023-06-02 13:50:59 -0500

    Trigger new build

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,6 @@
 import Footer from "components/footer";
 import Head from "next/head";
 import Header from "components/header";
-import Image from "next/image";
 
 export const siteTitle = "Urge Overkill";
 
